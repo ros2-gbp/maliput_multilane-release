9d28f4b2e81b97698cab1cc02c8fbbb1fd187cd0
Franco Cipollone <franco.c@ekumenlabs.com>
2022-07-01 17:34:58 -0300

    Rebase from 'upstream'

--- src/maliput_multilane_test_utilities/fixtures.cc
+++ src/maliput_multilane_test_utilities/fixtures.cc
@@ -48,20 +48,17 @@ namespace multilane {
 constexpr char MULTILANE_RESOURCE_VAR[] = "MULTILANE_RESOURCE_ROOT";
 
 BranchAndMergeBasedTest::BranchAndMergeBasedTest()
-    : road_geometry_(LoadFile(BuilderFactory(), maliput::common::Filesystem::get_env_path(MULTILANE_RESOURCE_VAR) +
-                                                    "/branch_and_merge.yaml")),
+    : road_geometry_(LoadFile(BuilderFactory(), std::string(DEF_MULTILANE_RESOURCE_ROOT) + "/branch_and_merge.yaml")),
       index_(road_geometry_->ById()),
       total_length_(index_.GetLane(LaneId("l:1.1_0"))->length() + index_.GetLane(LaneId("l:1.2_0"))->length() +
                     index_.GetLane(LaneId("l:1.3_0"))->length()) {}
 
 LoopBasedTest::LoopBasedTest()
-    : road_geometry_(
-          LoadFile(BuilderFactory(), maliput::common::Filesystem::get_env_path(MULTILANE_RESOURCE_VAR) + "/loop.yaml")),
+    : road_geometry_(LoadFile(BuilderFactory(), std::string(DEF_MULTILANE_RESOURCE_ROOT) + "/loop.yaml")),
       index_(road_geometry_->ById()) {}
 
 MultiBranchBasedTest::MultiBranchBasedTest()
-    : road_geometry_(LoadFile(
-          BuilderFactory(), maliput::common::Filesystem::get_env_path(MULTILANE_RESOURCE_VAR) + "/multi_branch.yaml")),
+    : road_geometry_(LoadFile(BuilderFactory(), std::string(DEF_MULTILANE_RESOURCE_ROOT) + "/multi_branch.yaml")),
       index_(road_geometry_->ById()) {}
 
 }  // namespace multilane
